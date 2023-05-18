8170793e2a81aae3a852ae36146865dd3c530730
PapaPeskwo <58036451+PapaPeskwo@users.noreply.github.com>
2023-05-18 16:13:21 +0200

    works, but seems to take more time now

--- file_folder_info.py
+++ file_folder_info.py
@@ -8,46 +8,31 @@ def convert_size(size_bytes):
     i = int(math.floor(math.log(size_bytes, 1024)))
     p = math.pow(1024, i)
     s = round(size_bytes / p, 2)
-    return f'{s} {size_name[i]}'
-
-def get_file_info(folder_path, extensions=None):
-    file_info = {}
-
-    for root, dirs, files in os.walk(folder_path):
-        for file in files:
-            file_path = os.path.join(root, file)
-            file_ext = os.path.splitext(file)[1].lower()
-
-            # This line removes leading '.' from the extension
-            file_ext = file_ext[1:] if file_ext.startswith('.') else file_ext
-
-            if extensions and file_ext not in extensions:
-                continue
-
-            if file_ext not in file_info:
-                file_info[file_ext] = {
-                    'count': 1,
-                    'total_size': os.path.getsize(file_path)
-                }
-            else:
-                file_info[file_ext]['count'] += 1
-                file_info[file_ext]['total_size'] += os.path.getsize(file_path)
-
-    return file_info
+    return f"{s} {size_name[i]}"
 
 def print_file_info(file_info):
-    for file_ext, info in file_info.items():
+    sorted_file_info = sorted(file_info.items(), key=lambda item: item[1]['total_size'], reverse=True)
+    for extension, info in sorted_file_info:
+        count = f"{info['count']:,}"
         total_size = convert_size(info['total_size'])
-        formatted_count = format(info['count'], ',')  # Adds comma as thousand separator
-        print(f"{formatted_count} {file_ext} files with total size: {total_size}")
+        print(f"{count} {extension} files with total size: {total_size}")
 
-folder_path = input("Please enter the path of the folder: ")
-extensions_input = input("Please enter the file extensions to look for, separated by comma (or leave empty to consider all files): ")
+def main():
+    folder_path = input("Please enter the path of the folder: ")
+    file_types_input = input("Please enter the file extensions to look for, separated by comma (or leave empty to consider all files): ")
+    file_types = [ft.strip().lower() for ft in file_types_input.split(",")] if file_types_input else None
+    file_info = {}
+    for foldername, subfolders, filenames in os.walk(folder_path):
+        for filename in filenames:
+            if not file_types or any(filename.lower().endswith(ft) for ft in file_types):
+                extension = os.path.splitext(filename)[1].lower()
+                file_size = os.path.getsize(os.path.join(foldername, filename))
+                if extension in file_info:
+                    file_info[extension]['count'] += 1
+                    file_info[extension]['total_size'] += file_size
+                else:
+                    file_info[extension] = {'count': 1, 'total_size': file_size}
+    print_file_info(file_info)
 
-if extensions_input:
-    extensions = [ext.strip().lower() for ext in extensions_input.split(",")]
-else:
-    extensions = None
-
-file_info = get_file_info(folder_path, extensions)
-print_file_info(file_info)
+if __name__ == "__main__":
+    main()
